a5093076d54386ad4e6ee4c9d5706a76c2bb2c9a
erikqtrs <erik.anndy@gmail.com>
2022-07-07 21:20:05 -0500

    Extract data with a scraper

---
+++ data/scraper.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import cloudscraper
+import time
+scraper = cloudscraper.create_scraper(delay=10)
+price_list = []
+feature_list = []
+location_list = []
+for page in range(1,129):
+    url = f'https://www.plusvalia.com/casas-en-venta-en-quito-hasta-10-anos-pagina-{page}.html'
+    info = scraper.get(url).text
+    soup = BeautifulSoup(info, 'lxml')
+    prices = soup.find_all('div', attrs={ 'data-qa':'POSTING_CARD_PRICE'  })
+    features = soup.find_all('div', attrs = {'data-qa':"POSTING_CARD_FEATURES"})
+    locations = soup.find_all('div', attrs={ 'data-qa':'POSTING_CARD_LOCATION' })
+    for price in prices:
+        price_list.append(price.get_text())
+    for feature in features:
+        feature_list.append(feature.get_text())
+    for location in locations:
+        location_list.append(location.get_text())
+    time.sleep(0.02)
+
+
+df = pd.DataFrame({'features': feature_list, 'location': location_list, 'price': price_list})
+
+df.to_csv('houses.csv', index=False)
